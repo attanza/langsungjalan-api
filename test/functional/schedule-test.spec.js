094b8ce00de1a4049a08458782a52ba5a01abbb4
Dani Lesmiadi <dani@foodgasm.id>
2018-05-28 18:33:46 +0700

    Schedule List Test

--- test/functional/schedule-test.spec.js
+++ test/functional/schedule-test.spec.js
@@ -1,7 +1,31 @@
 'use strict'
 
-const { test } = use('Test/Suite')('Schedule Test')
 
-test('make sure 2 + 2 is 4', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
+const Schedule = use('App/Models/Schedule')
+const User = use('App/Models/User')
+
+const { test, trait } = use('Test/Suite')('Schedule')
+
+trait('Test/ApiClient')
+trait('DatabaseTransactions')
+trait('Auth/Client')
+
+/**
+ * List of Role
+ */
+
+test('Unathorized cannot get Schedule List', async ({ client }) => {
+  const response = await client
+    .get('/api/v1/schedules')
+    .end()
+  response.assertStatus(401)
+})
+
+test('Authorized can get Schedule List', async ({ client }) => {
+  const user = await User.find(1)
+  const response = await client
+    .get('/api/v1/schedules')
+    .loginVia(user, 'jwt')
+    .end()
+  response.assertStatus(200)
 })
