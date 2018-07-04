816a9c2eb602c11e399f7868db850c1d0b357767
Dani Lesmiadi <dani@foodgasm.id>
2018-07-04 21:10:56 +0700

    Get Permissions Combo Data

--- app/Controllers/Http/ComboDataController.js
+++ app/Controllers/Http/ComboDataController.js
@@ -1,29 +1,35 @@
 'use strict'
 
+const { RedisHelper, ResponseParser } = use('App/Helpers')
 const University = use('App/Models/University')
-const { RedisHelper } = use('App/Helpers')
 const User = use('App/Models/User')
-
+const Permission = use('App/Models/Permission')
 
 class ComboDataController {
   async index({ request, response }) {
     const { model } = request.get()
     switch (model) {
-      case 'University':
-        {
-          const universities = await this.getUniversities()
-          return response.status(200).send(universities)
-        }
+    case 'University':
+    {
+      const data = await this.getUniversities()
+      return response.status(200).send(data)
+    }
 
-      case 'Marketing':
-        {
-          const marketings = await this.getMarketings()
-          return response.status(200).send(marketings)
-        }
+    case 'Marketing':
+    {
+      const data = await this.getMarketings()
+      return response.status(200).send(data)
+    }
+
+    case 'Permission':
+    {
+      const data = await this.getPermissions()
+      return response.status(200).send(data)
+    }
 
 
-      default:
-        return response.status(400).send({ 'message': 'Model not found' })
+    default:
+      return response.status(400).send({ 'message': 'Model not found' })
     }
   }
 
@@ -36,7 +42,8 @@ class ComboDataController {
     }
     const data = await University.query().select('id', 'name').orderBy('name').fetch()
     await RedisHelper.set(redisKey, data)
-    return data
+    let parsed = ResponseParser.apiItem(data.toJSON())
+    return parsed
   }
 
   async getMarketings() {
@@ -52,8 +59,21 @@ class ComboDataController {
       .orderBy('name')
       .fetch()
     await RedisHelper.set(redisKey, data)
-    return data
+    let parsed = ResponseParser.apiItem(data.toJSON())
+    return parsed
+  }
 
+  async getPermissions() {
+    let redisKey = 'Permission_Combo'
+    let cached = await RedisHelper.get(redisKey)
+
+    if (cached != null) {
+      return cached
+    }
+    const data = await Permission.query().select('id', 'name').fetch()
+    await RedisHelper.set(redisKey, data)
+    let parsed = ResponseParser.apiItem(data.toJSON())
+    return parsed
   }
 }
 
