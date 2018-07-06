7b6dd48f695bf67816d9f674c2d792e7db83e7a7
Dani Lesmiadi <dani@foodgasm.id>
2018-07-06 19:16:22 +0700

    Refactoring Schedule Start time data type to dateTime

--- app/Models/Schedule.js
+++ app/Models/Schedule.js
@@ -3,6 +3,11 @@
 const Model = use('Model')
 
 class Schedule extends Model {
+
+  static get dates () {
+    return super.dates.concat(['start_date', 'end_date'])
+  }
+  
   marketing() {
     return this.belongsTo('App/Models/User', 'marketing_id')
   }
