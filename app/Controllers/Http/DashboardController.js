d9d403b9032f6a7b448863f86c573bf2d0cc7902
Dani Lesmiadi <dani@foodgasm.id>
2018-09-09 14:37:46 +0700

    Refactoring DashboardController

--- app/Controllers/Http/DashboardController.js
+++ app/Controllers/Http/DashboardController.js
@@ -6,7 +6,6 @@ const User = use('App/Models/User')
 const Product = use('App/Models/Product')
 const University = use('App/Models/University')
 
-
 class DashboardController {
   async index({ response }) {
     const redisKey = 'Dashboard_Data'
@@ -20,29 +19,27 @@ class DashboardController {
     return response.status(200).send(dashboard)
   }
 
-  async store({response}) {
+  async store({ response }) {
     const totalMarketings = await User.query()
-      .whereHas('roles', (builder) => {
+      .whereHas('roles', builder => {
         builder.where('slug', 'marketing')
       })
       .where('is_active', 1)
       .count('* as total')
 
     const totalProducts = await Product.query().count('* as total')
-    const totalUniversities = await University.query().count('* as total')
 
+    const totalUniversities = await University.query().count('* as total')
 
     const dashboardDetails = {
       total_marketings: totalMarketings[0].total,
       total_products: totalProducts[0].total,
-      total_universities: totalUniversities[0].total,
+      total_universities: totalUniversities[0].total
     }
 
-    const whereClause = {
-      id: 1
-    }
-
-    const dashboard = await Dashboard.findOrCreate(whereClause, dashboardDetails)
+    const dashboard = await Dashboard.first()
+    dashboard.merge(dashboardDetails)
+    await dashboard.save()
 
     const redisKey = 'Dashboard_Data'
     await RedisHelper.delete(redisKey)
