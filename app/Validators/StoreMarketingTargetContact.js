b1191143a64b1ecfdab6a9fe329775fce44ee9d3
attanza <myrbx.747@gmail.com>
2018-12-06 05:37:19 +0700

    Email not required

--- app/Validators/StoreMarketingTargetContact.js
+++ app/Validators/StoreMarketingTargetContact.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict"
 
-const { ResponseParser } = use('App/Helpers')
-const messages = require('./messages')
+const { ResponseParser } = use("App/Helpers")
+const messages = require("./messages")
 
 class StoreMarketingTargetContact {
   get rules() {
     return {
-      name: 'required|max:50',
-      title: 'required|max:50',
-      phone: 'required|max:50',
-      email: 'required|email',
-      marketing_target_id: 'required|integer'
+      name: "required|max:50",
+      title: "required|max:50",
+      phone: "required|max:50",
+      email: "email",
+      marketing_target_id: "required|integer",
     }
   }
 
@@ -18,19 +18,20 @@ class StoreMarketingTargetContact {
     return messages
   }
 
-  get sanitizationRules () {
+  get sanitizationRules() {
     return {
-      name: 'escape',
-      title: 'escape',
-      phone: 'escape',
-      email: 'escape',
-      marketing_target_id: 'toInt',
-
+      name: "escape",
+      title: "escape",
+      phone: "escape",
+      email: "escape",
+      marketing_target_id: "toInt",
     }
   }
 
   async fails(errorMessages) {
-    return this.ctx.response.status(422).send(ResponseParser.apiValidationFailed(errorMessages))
+    return this.ctx.response
+      .status(422)
+      .send(ResponseParser.apiValidationFailed(errorMessages))
   }
 }
 
